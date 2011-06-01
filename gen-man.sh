d8ca4df80b47b2a4045d114d8ff83782f3f21884
kollerma <koller@stat.math.ethz.ch>
2011-06-01 14:30:49 +0200

    Added script that generates man pages.

---
+++ gen-man.sh
@@ -0,0 +1,95 @@
+#!/bin/bash -e
+## Generate man pages for git commands in this directory.
+## just start this script in the directory of the commands
+## and it will create a folder man/man1 and put the generated
+## manpages there.
+
+## call script --help
+## parse help
+## write man page
+
+## get current date
+date=`date +"%B %d, %Y"`
+## mk man/man1 dir
+mkdir -p man/man1
+
+for CMD in git-*;
+do
+    man="man/man1/$CMD.1"
+    desc=
+    synopsis=
+    options=
+    usage=0
+    echo "Creating man page for $CMD..."
+    help=`$CMD --help`
+    while IFS="" read line; do
+	## remove one leading whitespace
+	line=${line# }
+	## remove trailing whitespaces
+	line=${line%% }
+	## remove any leading whitespaces
+	cline=`echo $line`
+	if [[ "$usage" -eq 2 ]]; then
+	    ## we're in options
+	    if [[ "${cline:1:1}" == "-" ]]; then
+	    options="$options
+$cline"
+	    else
+		options="$options $cline"
+	    fi
+	elif [[ "$usage" -eq 1 ]]; then
+	    ## we're in Synopsis
+	    if [[ "$cline" == "" ]]; then
+		usage=2
+	    else
+		synopsis="$synopsis $cline"
+	    fi
+	else
+	    if expr "$line" : 'Usage:' > /dev/null; then
+		usage=1
+	    elif [[ "$cline" != "" ]]; then
+		if [[ "$cline" == "$line" ]]; then
+		    desc="$desc 
+$line"
+		else
+		    desc="$desc
+$line
+.br"
+		fi
+	    fi
+	fi	    
+    done <<< "$help"
+
+    ## remove leading whitespace, TODO: quote dashes
+    synopsis=${synopsis:1}
+    options=${options:1}
+    desc=${desc:1}
+
+    ## remove old version if available
+    if [ -f "$man.gz" ]; then
+	rm "$man.gz"
+    fi
+
+    ## write man page
+    echo ".TH $CMD 1 \"$date\"" > "$man"
+    echo ".SH NAME" >> "$man"
+    echo "$CMD" >> "$man"
+    echo ".SH SYNOPSIS" >> "$man"
+    echo ".B $synopsis" >> "$man"
+    echo ".SH DESCRIPTION" >> "$man"
+    echo "$desc" >> "$man"
+    if [[ "$options" != "" ]]; then
+	echo ".SH OPTIONS" >> "$man"
+	while read line; do
+	    echo ".TP" >> "$man"
+	    echo "${line%% *}" >> "$man"
+	    echo "${line#* }" >> "$man"
+	done <<< "$options"
+    fi
+    echo ".SH AUTHOR" >> "$man"
+    echo "Manuel Koller" >> "$man"
+    
+    ## gzip it
+    gzip "$man"
+done
+
