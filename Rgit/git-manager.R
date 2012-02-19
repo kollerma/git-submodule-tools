87c1b2f1503fc5fffdb524a5bd7d2f1b05c570a6
Manuel Koller <koller@stat.math.ethz.ch>
2012-02-19 18:27:50 +0100

    Added readRepo() function, improved gitStatus(), added gitStatus2Str().
    readRepo() combines the information of gitLsFiles() and gitStatus() into one data.frame.
    gitStatus() now returns a data.frame() with the status codes and the filenames.

--- Rgit/git-manager.R
+++ Rgit/git-manager.R
@@ -1,6 +1,51 @@
 require(gWidgets)
 require(gWidgetsRGtk2)
+require(gtools) ## for mixedsort
 ## source("methods.R")
+
+##' readRepo
+##'
+##' Function to read in the files and status of a 
+##' git repository.
+##' @param dir repository directory
+##' @return data.frame
+readRepo <- function(dir=getwd()) {
+  ## files in stage
+  files <- gitLsFiles(dir, "stage")
+  ## get status (also shows ignored and untracked files)
+  status <- gitStatus(dir, ignored=TRUE)
+  ## add ignored and untracked files to files
+  if ("??" %in% status$XY)
+    files <- rbind(files,
+                   data.frame(tag = "", mode = NA, object = "", stage = NA,
+                              file = with(status, file[XY == "??"])))
+  if ("!!" %in% status$XY)
+    files <- rbind(files,
+                   data.frame(tag = "", mode = NA, object = "", stage = NA,
+                              file = with(status, file[XY == "!!"])))
+  ## add directories
+  files$directory = sub("/[^/]*$", "/", files$file)
+  files <- within(files, directory[directory == file] <- "")
+  dirs <- unique(files$directory)
+  dirs <- dirs[dirs != ""]
+  if (length(dirs) > 0) 
+    files <- rbind(files,
+                   data.frame(tag = "", mode = 040000, object = "", stage = NA,
+                              file = dirs, directory = sub("/[^/]*/", "/", dirs)))
+  ## add status
+  files$was <- files$status <- ""
+  for (file in status$file) {
+    idx1 <- status$file == file
+    idx2 <- files$file == file
+    files$status[idx2] <- status$XY[idx1]
+    files$was[idx2] <- status$was[idx1]
+  }
+  ## sort by directory, filename
+  if (nrow(files) > 1) files <- files[with(files, mixedorder(file)),]
+  ## remove directory from filename
+  files$filename <- sub(".*/([^/]*/?)$", "\\1", files$file)
+  files
+}
 
 ## function to find offspring
 offspring <- function(path, user.data=NULL) {
@@ -11,8 +56,10 @@ offspring <- function(path, user.data=NULL) {
   else
     directory <- getwd()
   
-  files <- gitLsFiles(directory, c("stage", "others"))
-  data.frame(Filename=files$file,
+  files <- readRepo(directory)
+  ## drop all files in directories
+  files <- files[files$directory == "", ]
+  data.frame(Filename=files$filename, Status = gitStatus2Str(files$status),
              Mode=gitMode2Str(files$mode), mode=files$mode)
 }
 hasOffspring <- function(children,user.data=NULL, ...) {
@@ -20,13 +67,10 @@ hasOffspring <- function(children,user.data=NULL, ...) {
 }
 icon.FUN <- function(children,user.data=NULL, ...) {
   x <- rep("file", length=nrow(children))
-  str(children)
   x[children$mode == 160000] <- "directory"
   return(x)
 }
 
-## does not show isdir directory, as hasOffspring=NULL and
-## second column is a logical
 w <- gwindow("GUI Test", toolkit=guiToolkit("RGtk2"))
 tr <- gtree(offspring, hasOffspring, icon.FUN = icon.FUN, container=w)
 
--- Rgit/methods.R
+++ Rgit/methods.R
@@ -14,6 +14,7 @@ gitSystem <- function(args, dir) {
   ## run command
   #print(args)
   res <- system2("git", args=args, stdout=TRUE, stderr=TRUE)
+  #print(res)
   ## restore working dir
   if (!missing(dir)) setwd(ldir)
   return(res)
@@ -26,19 +27,32 @@ gitSystem <- function(args, dir) {
 ##' @param untracked (see git status documentation)
 ##' @param ignoreSubmodules (see git status documentation)
 ##' @param ignored show ignored files (see git status documentation)
-##' @return vector of output
+##' @return data.frame with three columns:
+##'   XY (status code), file, was (original name in case of rename)
 gitStatus <- function(dir=getwd(),
                       untracked = c("all", "no", "normal"),
                       ignoreSubmodules = c("none", "untracked", "dirty", "all"),
-                      ignored = TRUE) {
+                      ignored = FALSE) {
   ## match arguments
   untracked <- match.arg(untracked)
   ignoreSubmodules <- match.arg(ignoreSubmodules)
   ## build command
   args = sprintf("status --porcelain --untracked=%s --ignore-submodules=%s%s",
-    untracked, ignoreSubmodules, if (!ignored) " --ignored" else "")
+    untracked, ignoreSubmodules, if (ignored) " --ignored" else "")
   ## get status
-  res <- gitSystem(args, dir)
+  status <- gitSystem(args, dir)
+  if (length(status) == 0) return(data.frame())
+  ## convert into data.frame
+  lclean <- function(lst) {
+    ret <- matrix("", length(lst), 2)
+    colnames(ret) <- c("file", "was")
+    ret[,1] <- sapply(lst, function(x) if (length(x) > 1) x[2] else x[1])
+    ret[,2] <- sapply(lst, function(x) if (length(x) > 1) x[1] else "")
+    gsub("\\\"", "", ret)
+  }
+  res <- data.frame(XY=substring(status, 1, 2),
+                    lclean(strsplit(substring(status, 4), " -> ")),
+                    stringsAsFactors=FALSE)
   res
 }
 
@@ -116,4 +130,64 @@ gitMode2Str <- function(mode) {
                                   `120000` = "Symbolic link",
                                   `160000` = "Submodule (Gitlink)",
                                   "unknown"))
+}
+
+##' gitStatus2Str
+##'
+##' Convert status code (XZ) to a human readable string.
+##' @param status vector of status codes to convert.
+##' @return vector of strings.
+gitStatus2Str <- function(status) {
+  ## from the git documentation (man git-status)
+  ##   X          Y     Meaning
+  ## -------------------------------------------------
+  ##           [MD]   not updated
+  ## M        [ MD]   updated in index
+  ## A        [ MD]   added to index
+  ## D         [ M]   deleted from index
+  ## R        [ MD]   renamed in index
+  ## C        [ MD]   copied in index
+  ## [MARC]           index and work tree matches
+  ## [ MARC]     M    work tree changed since index
+  ## [ MARC]     D    deleted in work tree
+  ## -------------------------------------------------
+  ## D           D    unmerged, both deleted
+  ## A           U    unmerged, added by us
+  ## U           D    unmerged, deleted by them
+  ## U           A    unmerged, added by them
+  ## D           U    unmerged, deleted by us
+  ## A           A    unmerged, both added
+  ## U           U    unmerged, both modified
+  ## -------------------------------------------------
+  ## ?           ?    untracked
+  ## !           !    ignored
+  ## -------------------------------------------------
+  ret <- sapply(status, function(xy)
+                switch(xy,
+                       `DD` = "unmerged, both deleted",
+                       `AU` = "unmerged, added by us",
+                       `UD` = "unmerged, deleted by them",
+                       `UA` = "unmerged, added by them",
+                       `DU` = "unmerged, deleted by us",
+                       `AA` = "unmerged, both added",
+                       `UU` = "unmerged, both modified",
+                       `??` = "untracked",
+                       `!!` = "ignored",
+                       ## otherwise
+                       {
+                         if (xy %in% c("", " ")) xy <- "  "
+                         paste(switch(substring(xy, 1, 1),
+                                      M="modified in index",
+                                      A="added to index",
+                                      D="deleted from index",
+                                      R="renamed in index",
+                                      C="copied in index",
+                                      ` `="", "unknown code"),
+                               switch(substring(xy, 2, 2),
+                                      M="modified in work tree",
+                                      D="deleted in work tree",
+                                      ` `="", "unknown code"), sep=", ")
+                       }))
+ ret <- sub("^, ", "", ret)
+ sub(", $", "", ret)
 }
