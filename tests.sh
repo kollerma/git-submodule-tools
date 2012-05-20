82289eadc8010b58e33472afc42223bc86071440
Manuel Koller <koller@stat.math.ethz.ch>
2012-05-20 14:17:49 +0200

    Forcing bash to stop on errors in subshells.
    What is set -e for if it does not catch errors in subshells as well?

--- tests.sh
+++ tests.sh
@@ -64,13 +64,13 @@ mkdir vorlesung
     git submodule add $wd/remote/serie2.git serie2
     git submodule add $wd/remote/r-tutorial.git r-tutorial
     git commit -m 'first commit'
-)
+) || exit 1
 git clone --bare vorlesung remote/vorlesung.git
 (cd vorlesung
     git remote add origin $wd/remote/vorlesung.git
     git tag state-0
     git push --tag
-)
+) || exit 1
 
 ## clone vorlesung.git to vorlesung and vorlesung2 
 rm -rf vorlesung
@@ -86,16 +86,16 @@ git rclone $wd/remote/vorlesung.git vorlesung3
 (cd vorlesung2
     (cd r-tutorial
 	echo "some random content" >> tutorial.Rnw
-    )
+    ) || exit 1
     (cd serie1/aufgabe1
     echo "try to do it today" >> ex.Rnw
-    )
+    ) || exit 1
     ## edit aufgabe2 but do commit
     (cd serie1/aufgabe2
     	echo "try again" >> ex.Rnw
     	git add ex.Rnw
     	git commit -m 'This is no problem'
-    )
+    ) || exit 1
     ## rcommit without -a should warn of unstaged changes now
     git rcommit -m "I did something"
     ## ok, then add files that are already tracked
@@ -105,7 +105,7 @@ git rclone $wd/remote/vorlesung.git vorlesung3
     ## Add a tag as well
     git tag state-1
     git push --tag
-)
+) || exit 1
 
 ## pull this in vorlesung
 (cd vorlesung
@@ -113,7 +113,7 @@ git rclone $wd/remote/vorlesung.git vorlesung3
     ## This should report that there are updates available
     ## for serie2/aufgabe1 which points to the same rep. as
     ## serie1/aufgabe2.
-)
+) || exit 1
 
 ## clone vorlesung to vorlesung4
 git rclone $wd/remote/vorlesung.git vorlesung4
@@ -131,10 +131,10 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     ## force a conflict
     (cd r-tutorial
 	echo "some more content" >> tutorial.Rnw
-    )
+    ) || exit 1
     (cd serie2/aufgabe1
 	echo "we work against each other" >> ex.Rnw
-    )
+    ) || exit 1
     ## try to pull now, should fail
     git rpull && exit 1 || echo "rpull failed, as required."
     ## try to push, should fail, since there are new commits
@@ -156,18 +156,18 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     ## merge all differences
     (cd r-tutorial
 	git pull && exit 1 || echo "corrected version" > tutorial.Rnw
-    )
+    ) || exit 1
     ## This can be fast forwarded
     (cd serie1/aufgabe1
 	git pull
-    )
+    ) || exit 1
     ## This can be fast forwarded
     (cd serie1/aufgabe2
 	git pull
-    )
+    ) || exit 1
     (cd serie2/aufgabe1
 	git pull && exit 1 || cat ex.Rnw | sed -e '7 d' -e '5 d' -e '3 d' > ex.Rnw
-    )
+    ) || exit 1
     ## Now git rdiff should not show any changes
     git rdiff
     ## ok, commit then
@@ -181,7 +181,7 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     git rpush || { echo "rpush failed, this shouldn't happen!!"; exit 1; }
     ## remove the file again
     rm serie1/aufgabe2/blah.Rnw
-)
+) || exit 1
 
 ## Now pull this in vorlesung
 (cd vorlesung
@@ -191,11 +191,11 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     ## update serie1/aufgabe2
     (cd serie1/aufgabe2
 	git pull
-    )
+    ) || exit 1
     ## commit and push this
     git rcommit -am 'updated serie1/aufgabe2'
     git rpush
-)
+) || exit 1
 
 ####
 ## try tag switching and removing submodules
@@ -217,13 +217,13 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     ## try to do stuff
     (cd r-tutorial
 	echo "testing..." >> tutorial.Rnw
-    )
+    ) || exit 1
     git rcommit -am 'testing stuff' && exit 1 || echo "rcommit failed as it should have"
     git rcheckout state-1 && exit 1 || echo "rcheckout failed as it should have"
     ## revert
     (cd r-tutorial
 	git reset --hard HEAD
-    )
+    ) || exit 1
     git rcheckout state-2
 
     ## remove some submodules and switch tags
@@ -237,16 +237,16 @@ git rclone $wd/remote/vorlesung.git vorlesung4
 	    git add hallo
 	    git commit -m 'catch this!'
 	    git checkout master
-	)
+	) || exit 1
 	git rm-submodule aufgabe2 && exit 1 || echo "rm-submodule caught non-tracking-branch"
 	## so merge it to master and push
 	(cd aufgabe2
 	    git merge testbranch
 	    git push
-	)
+	) || exit 1
 	## now it should work
     	git rm-submodule aufgabe2 || { echo "rm-submodule failed!"; exit 1; }
-    )
+    ) || exit 1
     ## have to commit this in super repository by hand
     git rcommit -am 'removed submodule serie2/aufgabe2'
     ## revert to state-2
@@ -257,7 +257,7 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     [ ! -f serie2/aufgabe2/ex.Rnw ] || { echo "Error: serie2/aufgabe2 not removed"; exit 1; }
     ## now push this
     git rpush
-)
+) || exit 1
 
 ####
 ## cause conflict -- stage2: 
@@ -270,7 +270,7 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     (cd serie2/aufgabe2
 	## there are already unpulled changes...
 	git pull
-    )
+    ) || exit 1
     ## should open an editor
     ## git rcommit -a
     git rcommit -am "playing with fire!!"
@@ -281,7 +281,7 @@ git rclone $wd/remote/vorlesung.git vorlesung4
     git rfetch --dry-run
     git rfetch
     git rdiff
-)
+) || exit 1
 ## make backup first
 cp -r vorlesung3 vorlesung3a
 ## continue...
@@ -290,13 +290,13 @@ cp -r vorlesung3 vorlesung3a
     (cd serie2
     	git converge-submodules
 	[ ! -f aufgabe2/ex.Rnw ] || { echo "Error: serie2/aufgabe2 not removed"; exit 1; }
-    )
+    ) || exit 1
     git rcommit -am 'updated serie1 and serie2'
     git rpush
     ## add tag and push
     git tag state-3
     git push --tags
-)
+) || exit 1
 ## now try this again in vorlesung3a
 ## (slightly different scenario for converge-submodules)
 cp -r vorlesung3a vorlesung3b
@@ -304,10 +304,10 @@ cp -r vorlesung3a vorlesung3b
     (cd serie2
     	git converge-submodules
 	[ ! -f aufgabe2/ex.Rnw ] || { echo "Error: serie2/aufgabe2 not removed"; exit 1; }
-    )
+    ) || exit 1
     ## now commit
     git rcommit -am 'updated serie1 and serie2'
-)
+) || exit 1
 
 ####
 ## cause conflict -- stage3: when submodules have been locally added
@@ -321,7 +321,7 @@ cp -r vorlesung3a vorlesung3b
     (cd serie2
 	git submodule add $wd/remote/aufgabe1.git aufgabe2
 	git commit -m 'added aufgabe1.git as aufgabe2'
-    )
+    ) || exit 1
     git rcommit -am 'did some work in serie2'
     git rpush && exit 1 || echo "Ok, there was an error in serie2"
     ## ok, check for updates
@@ -332,10 +332,10 @@ cp -r vorlesung3a vorlesung3b
 	## ok, force it
 	## this essentially forces the submodule add we just made
 	git converge-submodules -f
-    )
+    ) || exit 1
     git rcommit -am 'resolved conflict in serie2'
     ## don't push, want to go even further...
-)
+) || exit 1
 
 ####
 ## cause conflict -- stage4: when submodules will be removed in remote 
@@ -351,7 +351,7 @@ cp -r vorlesung3a vorlesung3b
 	git rm-submodule aufgabe2
 	git submodule add $wd/remote/aufgabe1.git aufgabe2
 	git commit -m 'added aufgabe1.git as aufgabe2'
-    )
+    ) || exit 1
     git rcommit -am 'did some work in serie2'
     git rpush && exit 1 || echo "Ok, there was an error in serie2"
     ## ok, check for updates
@@ -363,10 +363,10 @@ cp -r vorlesung3a vorlesung3b
 	## but give the info we need to add it again.
 	## do so
 	git submodule add $wd/remote/aufgabe1.git aufgabe2
-    )
+    ) || exit 1
     git rcommit -am 'resolved conflict in serie2'
     git rpush
-)
+) || exit 1
 
 ####
 ## cause conflict -- stage 5: when submodules have been added twice
@@ -381,9 +381,9 @@ cp -r vorlesung3a vorlesung3b
 	git submodule add $wd/remote/aufgabe1.git aufgabe2
 	git submodule add $wd/remote/aufgabe3.git aufgabe3
 	git commit -m 'added aufgabe1.git as aufgabe2, aufgabe3.git as aufgabe3'
-    )
+    ) || exit 1
     git rcommit -am 'added some submodules in serie2'
-)
+) || exit 1
 
 cp -r vorlesung3 vorlesung3c
 (cd vorlesung3
@@ -391,10 +391,10 @@ cp -r vorlesung3 vorlesung3c
     git rfetch
     (cd serie2
 	git converge-submodules
-    )
+    ) || exit 1
     git rcommit -am 'converged submodules'
     git rpush
-)
+) || exit 1
     
 ####
 ## test submodule version conflict resolution
@@ -406,18 +406,18 @@ cp -r vorlesung3 vorlesung3c
     (cd serie2/aufgabe1
 	git branch alternative origin/alternative
 	git checkout alternative
-    )
+    ) || exit 1
     git rcommit -am 'switched branch in serie2/aufgabe1'
-)
+) || exit 1
 
 (cd vorlesung3
     ## revert to an old commit in serie2/aufgabe1
     (cd serie2/aufgabe1
 	git reset --hard HEAD~1
-    )
+    ) || exit 1
     git rcommit -am 'reset version in serie2/aufgabe1'
     git rpush
-)
+) || exit 1
 
 (cd vorlesung
     ## fetch updates
@@ -425,15 +425,15 @@ cp -r vorlesung3 vorlesung3c
     (cd serie2
 	git converge-submodules
 	## fails
-    )
+    ) || exit 1
     git rcommit -am 'merged serie2'
     git rpush
-)
+) || exit 1
 
 (cd vorlesung3
     ## pull
     git rpull
-)
+) || exit 1
 
 ####
 ## test mv-submodule
@@ -452,7 +452,7 @@ cp -r vorlesung3 vorlesung3c
 
     [ ! -d serie3 ] || { echo "Error: serie3 not removed"; exit 1; }
     [ -d serie2 ] || { echo "Error: serie2 not created"; exit 1; }
-)
+) || exit 1
 
 ####
 ## test removal and adding of a submodule of the same name
@@ -464,7 +464,7 @@ cp -r vorlesung3 vorlesung3c
 (cd remote
     ## clone aufgabe1 into aufgabe4
     git clone --bare aufgabe1.git aufgabe4.git
-)
+) || exit 1
 
 ## add some commits to aufgabe1
 git clone remote/aufgabe1.git aufgabe1
@@ -474,7 +474,7 @@ git clone remote/aufgabe1.git aufgabe1
     echo even-more-content >> ex.Rnw
     git commit -am "second time adding more content to aufgabe1"
     git push
-)
+) || exit 1
 
 ## add some commits to aufgabe2
 git clone remote/aufgabe4.git aufgabe4
@@ -484,7 +484,7 @@ git clone remote/aufgabe4.git aufgabe4
     echo even-more-content2 >> ex.Rnw
     git commit -am "second time adding more content to aufgabe4"
     git push
-)
+) || exit 1
 
 ## create new repos vorlesung5 and vorlesung6
 git rclone remote/vorlesung.git vorlesung5
@@ -495,12 +495,12 @@ git rclone remote/vorlesung.git vorlesung6
     git rpull
     (cd serie1/aufgabe1
 	git checkout master
-    )
+    ) || exit 1
     git rcommit -am "updated series1/aufgabe1"
     ## the next push should be ok 
     ## (even if HEAD in serie2/aufgabe2 is not attached)
     git rpush
-)
+) || exit 1
 
 ## update to newest version aufgabe1 in vorlesung6
 (cd vorlesung6
@@ -508,10 +508,10 @@ git rclone remote/vorlesung.git vorlesung6
     (cd serie1
 	git rm-submodule aufgabe1
 	git submodule add $wd/remote/aufgabe4.git aufgabe1
-    )
+    ) || exit 1
     git rcommit -am "added aufgabe4.git as aufgabe1"
     git rpush
-)
+) || exit 1
 
 ## go to vorlesung5 and pull again
 (cd vorlesung5
@@ -519,6 +519,6 @@ git rclone remote/vorlesung.git vorlesung6
     ## the head of serie1/aufgabe1 should be attached now
     (cd serie1/aufgabe1
 	git branch -v -a
-    )
-)
+    ) || exit 1
+) || exit 1
     
